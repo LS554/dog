9c97888af0f8590e53cc798edd87fcccbc7205f4
LS554 <103077889+LS554@users.noreply.github.com>
2025-07-22 14:32:51 -0400

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,6 @@
+// Copyright (c) 2025, London Sheard
+// Licensed under the 10-Clause Sheard License
+// See the LICENSE file in project root for details.
+//
+// Created by London Sheard on 22/07/25.
+//
